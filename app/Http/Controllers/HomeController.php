4a2388757d1dbead69bd6329bb347cb726d558f2
khoailang99 <khoailang19101999@gmail.com>
2020-11-02 11:34:09 +0700

    Lay danh sach loai va danh muc san pham tu DB.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProductType;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -13,7 +16,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
     }
 
     /**
@@ -23,6 +26,33 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $prodTypeList = array();
+        $listSubLvProd = array();
+        // $ancentralLevelProdT = $this -> getListProdTAccordTFather(0);
+        // foreach($ancentralLevelProdT as $prodT) {
+        //     array_push($prodTypeList, $prodT);
+        //     $prodTypeT = $this -> getListProdTAccordTFather($prodT->ID);
+        //     foreach($prodTypeT as $prodTT) {
+        //         array_push($prodTypeList, $prodTT);
+        //         array_push($prodTypeList, ...($this -> getListProdTAccordTFather($prodTT->ID)));
+        //     }
+        // }
+
+        $ancentralLevelProdT = $this -> getListProdTAccordTFather(0);
+        foreach($ancentralLevelProdT as $prodT) {
+            $aSuperPT = new ProductType($prodT, array());
+            $prodTypeT = $this -> getListProdTAccordTFather($prodT->ID);
+            foreach($prodTypeT as $prodTT) {
+                array_push($aSuperPT -> childLv, new ProductType($prodTT, $this -> getListProdTAccordTFather($prodTT->ID)));
+            }
+            array_push($prodTypeList, $aSuperPT);
+        }
+        \Debugbar::info($prodTypeList);
+        \Debugbar::warning('----');
+        return view('home', ['prodTypeList' => $prodTypeList]);
+    }
+
+    private function getListProdTAccordTFather($id) {
+        return DB::table('ProductCategories') -> where('ParentID', '=', $id) -> orderBy('DisplayOrder', 'asc') -> get() -> toArray();            
     }
 }
