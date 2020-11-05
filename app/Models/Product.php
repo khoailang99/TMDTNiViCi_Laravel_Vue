959853df7f835186bc94698d517feee385b89480
khoailang99 <khoailang19101999@gmail.com>
2020-11-06 00:53:45 +0700

    Them method lay tong so sp va method lay danh sach sp theo pagination.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,10 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-  private $prodNumbDisplayed = 30;
+  private $prodNumbDisplayed = 8;
+
+  public function getCountProd() {
+    return Product::count();
+  }
 
   public function getAllProductsPM() {
     $products = Product::where('CategoryID',1) -> take($this -> prodNumbDisplayed) -> get();
+    return $products;
+  }
+
+  public function getProdP($page) {
+    $products = Product::skip(($page - 1) * $this -> prodNumbDisplayed) -> take($this -> prodNumbDisplayed) -> get();
     return $products;
   }
 
