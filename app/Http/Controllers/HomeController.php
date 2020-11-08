d83a4a2a4777c1cc63efd60e0c70e11976f6c166
khoailang99 <khoailang19101999@gmail.com>
2020-11-08 18:38:28 +0700

    Cap nhat lai metho tra ve ds san pham o trang home va method getAllProdByPagination.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product\ProductTypeModel;
 use App\Models\Product\Product;
+use App\Models\Specification\ProductCategoryS_ProductS_Model;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -39,10 +40,7 @@ class HomeController extends Controller
         //     }
         // }
 
-        $grossProd = $product -> getCountProd();
-        $totalPages = ceil($grossProd / $product -> getProdNumbDisplayed());
         $ancentralLevelProdT = $this -> getListProdTAccordTFather(0);
-
         foreach($ancentralLevelProdT as $prodT) {
             $aSuperPT = new ProductTypeModel($prodT, array());
             $prodTypeT = $this -> getListProdTAccordTFather($prodT->ID);
@@ -51,9 +49,15 @@ class HomeController extends Controller
             }
             array_push($prodTypeList, $aSuperPT);
         }
-        \Debugbar::info($prodTypeList);
-        \Debugbar::warning('----');
-        return view('home', ['prodTypeList' => $prodTypeList, 'listProducts' => $product -> getAllProductsPM(), 'totalProd' => $grossProd, 'totalPages' => $totalPages ]);
+
+        $products_M = $product -> ProductsHomepage_PM();
+        $grossProd = $product -> totalNumbProdDisplayed();
+        $totalPages = ceil($grossProd / $product -> getProdNumbDisplayed());
+
+        \Debugbar::warning("Các sản phẩm ở trang home");
+        \Debugbar::info($products_M);
+        
+        return view('home', ['prodTypeList' => $prodTypeList, 'listProducts' => $products_M, 'totalProd' => $grossProd, 'totalPages' => $totalPages ]);
     }
 
     public function getAllProdByPagination(Request $request) {
@@ -61,7 +65,7 @@ class HomeController extends Controller
         $prodNumbDisplayed = $request -> input('prodNumbDisplayed');
         $product = new Product();
         
-        return array("listProducts" => $product -> getProdP($page));
+        return array("listProducts" => $product -> ProductsHomepage_PM($page));
     }
 
     private function getListProdTAccordTFather($id) {
