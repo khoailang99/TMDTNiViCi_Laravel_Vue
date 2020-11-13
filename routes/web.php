07f345327ef9a1b2ed79b6afe3bf1c4916f27ab6
khoailang99 <khoailang19101999@gmail.com>
2020-11-14 02:49:07 +0700

    Them route lay san pham theo bo loc, lay bo loc theo loai san pham.

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,17 @@
 
 Route::get('/', 'HomeController@index')->name('home');
 
+// Lấy các sản phẩm theo phân trang
 Route::get('/products', 'HomeController@getAllProdByPagination');
 
+// Lấy bộ lọc theo loại sản phẩm
+Route::get('/prod-catalog-filters', 'HomeController@getFilters_Category_ProdType');
+
+// Lấy sản phẩm theo bộ lọc
+Route::get('/prod-filters', 'HomeController@getProductsFilter');
+
+
+Route::get('/test', 'HomeController@demoTest') -> name('productCategory');
 
 Auth::routes();
 
