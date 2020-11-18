530d615d2a319289cebc0b784a771b430a856bd9
khoailang99 <khoailang19101999@gmail.com>
2020-11-18 14:42:45 +0700

    Su dung ProductDetailComponent.

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,11 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="css-5542cb">
-  <h4 style="font-size: 1rem"> Giao diện sản phẩm
-    <span style="font-size: 1.3rem; color: #bc1d1d;">
-      {{ $prodDetailM -> product -> Name }}
-    </span>
-    đang trong quá trình xây dựng!</h4>
-</div>
+  <product-detail-component :product_detail='@json($prodDetailM)'></product-detail-component>
 @endsection
